d654f399b2bf66a6760a121d31a27e5d9a6c3ff0
Ukulsum <ukulsum1280@gmail.com>
2023-07-18 03:12:27 +0600

    first commit

---
+++ CookiesDemo/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookiesDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            HttpCookie c = Request.Cookies["UserName"];
+            if(c != null)
+            {
+                ViewBag.Message = Request.Cookies["UserName"].Value.ToString();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ CookiesDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using CookiesDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookiesDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(User users)
+        {
+            if(ModelState.IsValid == true)
+            {
+                HttpCookie cookie = new HttpCookie("UserName");
+                cookie.Value = users.UserName;
+
+                HttpContext.Response.Cookies.Add(cookie);
+                cookie.Expires = DateTime.Now.AddDays(2);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            return View();
+        }
+    }
+}
